a05e7602ed2aafdb00f1a1e3fb17f3aada8c9f29
NomenjanaharyEric <rakotomaheryeric11@gmail.com>
2023-02-14 16:42:28 +0300

    Adding Pagination For Hotel view

--- src/Controller/HotelController.php
+++ src/Controller/HotelController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller;
 
+use App\Repository\HotelRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HotelController extends AbstractController
 {
-    #[Route('/hotel', name: 'app_hotel')]
-    public function index(): Response
+    #[Route('/hotel', name: 'app_hotel', methods:["GET"])]
+    /**
+     * Undocumented function
+     *
+     * @param HotelRepository $hotelRepository
+     * @param Request $request
+     * @param PaginatorInterface $paginator
+     * @return Response
+     */
+    public function index(HotelRepository $hotelRepository, Request $request, PaginatorInterface $paginator): Response
     {
-        return $this->render('pages/hotel/index.html.twig');
+        $hotels = $paginator->paginate(
+            $hotelRepository->findAll(),
+            $request->query->getInt("page", 1),
+            9
+        );
+        return $this->render('pages/hotel/index.html.twig', [
+            "hotels" => $hotels
+        ]);
     }
 }
