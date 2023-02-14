94029d2c46409ef1e8e96661f6c71a5c60dcfa2b
NomenjanaharyEric <rakotomaheryeric11@gmail.com>
2023-02-14 15:34:08 +0300

    Adding Hotel View

---
+++ src/Controller/HotelController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HotelController extends AbstractController
+{
+    #[Route('/hotel', name: 'app_hotel')]
+    public function index(): Response
+    {
+        return $this->render('pages/hotel/index.html.twig');
+    }
+}
